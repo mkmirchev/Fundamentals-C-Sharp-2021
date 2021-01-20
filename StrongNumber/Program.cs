11e4d697ff11a2b0e6a68b4110b718820006c835
mkmirchev <mkmirchev@abv.bg>
2021-01-20 11:58:47 +0200

    ttest

--- StrongNumber/Program.cs
+++ StrongNumber/Program.cs
@@ -8,7 +8,32 @@ namespace StrongNumber
         {
             string input = Console.ReadLine();
             int digits = input.Length;
+            int number = int.Parse(input);
+            int currentDigit = 1;
+            int helpDigit = number;
+            int fact = 1;
+            int sum = 0;
+            for (int i = 0; i < digits; i++)
+            {
+                currentDigit = helpDigit % 10;
+                //calculating factorial
+                for (int k = 1; k <= currentDigit; k++)
+                {
+                    fact = fact * k;
+                }
 
+                currentDigit = helpDigit / 10;
+                sum += fact;
+                fact = 1;
+            }
+            if(number == sum)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
         }
     }
 }

8bbd098bef79516c3de47c86d30e07fdcb725f41
mkmirchev <mkmirchev@abv.bg>
2021-01-20 11:43:23 +0200

    asd

---
+++ StrongNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int digits = input.Length;
+
+        }
+    }
+}

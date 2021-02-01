b68de555caee70683292dd0330b3f2336fc2e7c6
mkmirchev <mkmirchev@abv.bg>
2021-02-01 22:28:44 +0200

    Tast FromLeftToRightCompleted

---
+++ FromLeftToRight/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace FromLeftToRight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputRows = int.Parse(Console.ReadLine());
+            for (int i = 0; i < inputRows; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+                BigInteger firstNumber = BigInteger.Parse(input[0]);
+                
+                BigInteger secondNumber = BigInteger.Parse(input[1]);
+                if (firstNumber > secondNumber)
+                {
+                    Console.WriteLine(SumIntDigits(firstNumber));
+                }
+                else if (firstNumber <= secondNumber)
+                {
+                    Console.WriteLine(SumIntDigits(secondNumber)); 
+                }
+
+            }
+        }
+        static BigInteger SumIntDigits(BigInteger a)
+        {
+            BigInteger output = 0;
+            int count = a.ToString().Length;
+            for (int i = 0; i < count; i++)
+            {
+                output += a % 10;
+                a /= 10;
+            }
+            if (output < 0)
+            {
+                output *= -1;
+            }
+            return output;
+        }
+    }
+}

aa6eb768cd9a04cbf47fc9b062f05d3f8a079669
mkmirchev <mkmirchev@abv.bg>
2021-02-19 23:42:26 +0200

    Bitwise operations

---
+++ BinaryDigitCount/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryDigitCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int bit = int.Parse(Console.ReadLine());
+            List<int> lst = new List<int>();
+
+            while (true)
+            {
+                if (number / 2 > 0 && number % 2 == 0)
+                {
+                    lst.Add(0);
+                    number /= 2;
+                }
+                else if(number / 2 > 0 && number % 2 == 1)
+                {
+                    lst.Add(1);
+                    number /= 2;
+                }
+                if (number == 0)
+                {
+                    break;
+                }
+                else if (number == 1)
+                {
+                    lst.Add(1);
+                    break;
+                }
+            }
+            lst.Reverse();
+            int sum = 0;
+            foreach (var item in lst)
+            {
+                if (item == bit)
+                {
+                    sum += 1;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ TriBitSwitch/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TriBitSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            int mask = 7 << p;
+            Console.WriteLine(number^mask);
+
+        }
+    }
+}

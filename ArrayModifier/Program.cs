78f16b0cf158722721ffe38e3c68ed9fb736fded
mkmirchev <mkmirchev@abv.bg>
2021-03-04 11:15:30 +0200

    Mid Exam Practise

---
+++ ArrayModifier/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArrayModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputArray = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            while (true)
+            {
+                List<string> command = Console.ReadLine().Split(' ').ToList();
+                if (command[0] == "end")
+                {
+                    Console.WriteLine(string.Join(", ", inputArray));
+                    break;
+                }
+                if (command[0] == "swap")
+                {
+                    SwapArr(inputArray, int.Parse(command[1]), int.Parse(command[2]));
+                }
+                if (command[0] == "multiply")
+                {
+                    MultiplyElement(inputArray, int.Parse(command[1]), int.Parse(command[2]));
+                }
+                if (command[0] == "decrease")
+                {
+                    DecreaseAll(inputArray);
+                }
+            }
+
+           
+        }
+        static void SwapArr(List<int> input, int first, int second)
+        {
+            int temp = input[first];
+            input[first] = input[second];
+            input[second] = temp;
+        }
+        static void MultiplyElement(List<int> input, int first, int second)
+        {
+            input[first] = input[first] * input[second];
+        }
+        static void DecreaseAll(List<int> input)
+        {
+            for (int i = 0; i < input.Count; i++)
+            {
+                input[i] -= 1;
+            }
+        }
+    }
+}
---
+++ SoftUni Reception/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SoftUni_Reception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //reading inputs
+            int personOne = int.Parse(Console.ReadLine());
+            int personTwo = int.Parse(Console.ReadLine());
+            int personThree = int.Parse(Console.ReadLine());
+            int questions = int.Parse(Console.ReadLine());
+
+            //processing data
+            int count= 1;
+            int hours = 0;
+            int questionsPerHour = (personOne + personTwo + personThree);
+            while (true)
+
+            {   if (questions <= 0)
+                {
+                    break;
+                }
+                if (count%4==0)
+                {
+                    hours++;
+                }
+                else
+                {
+                    questions -= questionsPerHour;
+                    hours++;
+                }
+                count++;
+            }
+            //output
+            Console.WriteLine("Time needed: {0}h.",hours);
+            
+        }
+    }
+}

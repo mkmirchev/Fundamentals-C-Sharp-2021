30fdcda985fd5e6029cff035188d5dbd30d7708f
mkmirchev <mkmirchev@abv.bg>
2021-03-27 14:38:13 +0200

    Regex exercises

---
+++ Furniture/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @">>(?<furn>\w+)<<(?<price>\d+.\d{2}|\d+)!(?<quantity>\d+)";
+            List<string> lst = new List<string>();
+            decimal moneySpent = 0;
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Purchase")//exit condition
+                {
+                    Console.WriteLine("Bought furniture:");
+                    foreach (var item in lst)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    Console.WriteLine($"Total money spent: {moneySpent:F2}");
+                    break;
+                }
+                else
+                {
+                    if (Regex.IsMatch(input, pattern))
+                    {
+                        Match mtch = Regex.Match(input, pattern);
+                        string furn = mtch.Groups["furn"].Value;
+                        string price = mtch.Groups["price"].Value;
+                        string quantity = mtch.Groups["quantity"].Value;
+                        moneySpent += int.Parse(quantity) * decimal.Parse(price);
+                        lst.Add(furn);
+                    }                   
+                }
+               
+            }
+        }
+    }
+}
---
+++ Match phone numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Match_phone_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(\+359-2-\d{3}-\d{4}\b)|(\+359 2 \d{3} \d{4}\b)";
+            string input = Console.ReadLine();
+            MatchCollection mtch = Regex.Matches(input, pattern);
+            string[] strArr = mtch.Cast<Match>().Select(x => x.Value.Trim()).ToArray();
+            Console.WriteLine(string.Join(", ", strArr));
+        }
+    }
+}

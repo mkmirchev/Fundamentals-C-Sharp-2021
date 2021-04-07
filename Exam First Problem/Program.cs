b92d3eb2b1bcdd52ece4c2a1352f58886b8d8dd2
mkmirchev <mkmirchev@abv.bg>
2021-04-07 17:26:50 +0300

    practise

---
+++ Exam First Problem/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Exam_First_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(' ').ToArray();
+                if (input[0] == "Done")
+                {
+                    break;
+                }
+                if (input[0] == "Change")
+                {
+                    if (str.Contains(input[1]))
+                    {
+                        str = str.Replace((char)char.Parse(input[1]), (char)char.Parse(input[2]));
+                    }
+                    Console.WriteLine(str);
+                }
+                else if (input[0] == "Includes")
+                {
+                    if (str.Contains(input[1]))
+                    {
+                        Console.WriteLine("True");
+                    }
+                    else
+                    {
+                        Console.WriteLine("False");
+                    }
+                }
+                else if (input[0] == "End")
+                {
+                    if (str.EndsWith(input[1]))
+                    {
+                        Console.WriteLine("True");
+                    }
+                    else
+                    {
+                        Console.WriteLine("False");
+                    }
+                }
+                else if (input[0] == "Uppercase")
+                {
+                   str = str.ToUpper();
+                    Console.WriteLine(str);
+                }
+                else if (input[0] == "FindIndex")
+                {
+                    int a = str.IndexOfAny(input[1].ToCharArray());
+                    Console.WriteLine(a);
+                }
+                else if (input[0] == "Cut")
+                {
+                    string sub = str.Substring(int.Parse(input[1]), int.Parse(input[2]));
+                    str = sub;
+                    Console.WriteLine(str);
+                }
+            }
+        }
+    }
+}
---
+++ Star Enigma/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace Star_Enigma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lanes = int.Parse(Console.ReadLine());
+            string pattern = @"[STARstar]";
+            string encryption = @"([^\!\@\:\-\.]*)\@(?<planet>[A-Z][a-z]*)([^\!\@\:\-\.]*)\:(?<population>\d+)([^\!\@\:\-\.]*)\!(?<type>[AD])\!([^\!\@\:\-\.]*)->(?<army>\d+)([^\!\@\:\-\.]*)";
+            Regex rx = new Regex(pattern);
+            List<string> atacked = new List<string>();
+            List<string> destroyed = new List<string>();
+            for (int i = 0; i < lanes; i++)
+            {
+                string input = Console.ReadLine();
+                MatchCollection mh = rx.Matches(input);
+                int encryptKey = mh.Count;
+                string encryptedMsg = string.Empty;
+                for (int l = 0; l < input.Length; l++)
+                {
+                    encryptedMsg += (char)(input[l] - encryptKey);
+                }
+                Regex rgx = new Regex(encryption);
+                Match mtch = rgx.Match(encryptedMsg);
+                if (mtch.Groups["planet"].Value != string.Empty 
+                    && int.Parse(mtch.Groups["population"].Value) > 0
+                    && (char.Parse(mtch.Groups["type"].Value) == 'A' || char.Parse(mtch.Groups["type"].Value) == 'D')
+                    && int.Parse(mtch.Groups["army"].Value) > 0)
+                {
+                    string name = mtch.Groups["planet"].Value;
+                    int population = int.Parse(mtch.Groups["population"].Value);
+                    char ch = char.Parse(mtch.Groups["type"].Value);
+                    int armyCount = int.Parse(mtch.Groups["army"].Value);
+                    if (ch == 'A')
+                    {
+                        atacked.Add(name);
+                    }
+                    else
+                    {
+                        destroyed.Add(name);
+                    }
+                }
+
+            }
+            Console.WriteLine($"Attacked planets: {atacked.Count}");
+            atacked.Sort();
+            destroyed.Sort();
+            foreach (var item in atacked)
+            {
+                Console.WriteLine($"-> {item}");
+            }
+            Console.WriteLine($"Destroyed planets: {destroyed.Count}");
+            foreach (var item in destroyed)
+            {
+                Console.WriteLine($"-> {item}");
+            }
+        }
+    }
+}
